cb6fc9a26ca66c9117fb4790622fe0c425c08e9e
Jessica <duellje@oregonstate.edu>
2019-12-29 15:57:24 -0400

    Added line spacing, word spacing, aligned header background to extend full width of the page

--- myFirstPageStyle.cs
+++ myFirstPageStyle.cs
@@ -1,14 +1,34 @@
 
 font-family: arial, "Times New Roman", serif;
 
-body {
+html, body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
   font-size: 14px;
   color: navy;
+  overflow-x: hidden;
 }
 
-h1 {
-  color: #fbec5d;
-  background-color: #009;
+.full-width-bar{
+    margin: 0 -9999rem;
+    padding: 0.25rem 9999rem;
+    color: #fbec5d;
+    background-color: #009;
+}
+
+html{
+  display: table;
+  margin: auto;
+}
+
+body{
+  display: table-cell;
+  vertical-align: middle;
+  letter-spacing: 0.1em;
+  word-spacing: 0.1em;
+  line-height: 1.5;
 }
 
 
